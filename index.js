5e45d4df34cc89d6be9fe6751de9274a18871d98
MdTasib <ohidulalam442@gmail.com>
2022-05-11 13:13:06 +0600

    db connected and get all data from database

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +11,31 @@ app.use(express.json());
 app.get("/", (req, res) => {
 	res.send("Hello Doctors");
 });
+
+// mongodb uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rncvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const serviceCollection = client.db("doctorsPortal").collection("services");
+
+		// get all service form mongodb
+		app.get("/service", async (req, res) => {
+			const query = {};
+			const cursor = serviceCollection.find(query);
+			const services = await cursor.toArray();
+			res.send(services);
+		});
+	} finally {
+	}
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => console.log("server is runing"));
